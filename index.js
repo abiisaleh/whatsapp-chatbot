6586257a422c71c9e901073ce5647720a74d90be
Muhamad Abi Saleh <92934849+abiisaleh@users.noreply.github.com>
2023-12-23 10:57:33 +0900

    update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const qrcode = require("qrcode-terminal");
 
 const { Client, LocalAuth} = require("whatsapp-web.js");
 const client = new Client({
-  authStrategy: new LocalAuth()
+  authStrategy: new LocalAuth(),
   puppeteer: {
     args: ["--no-sandbox"],
   },
