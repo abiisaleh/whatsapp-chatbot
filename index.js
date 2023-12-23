66d558d6fc17bea169f452e6f8db9fca839b0d83
Muhamad Abi Saleh <92934849+abiisaleh@users.noreply.github.com>
2023-12-23 13:20:03 +0900

    update index.js

--- index.js
+++ index.js
@@ -2,12 +2,22 @@ require("dotenv").config({ path: "./.env" });
 const qrcode = require("qrcode-terminal");
 
 const fs = require('fs'); 
-const { Client, LocalAuth } = require('whatsapp-web.js'); 
+const { Client, LegacySessionAuth } = require('whatsapp-web.js'); 
+
+// Path where the session data will be stored 
+const SESSION_FILE_PATH = './session.json
+
+// Load the session data if it has been previously saved 
+let sessionData; 
+
+if(fs.existsSync(SESSION_FILE_PATH)) { 
+  sessionData = require(SESSION_FILE_PATH);
+} 
 
 // Use the saved values 
 const client = new Client({ 
-  authStrategy: new LocalAuth({ 
-    clientId: "amin" 
+  authStrategy: new LegacySessionAuth({
+    session: sessionData
   }),
   puppeteer: {
     args: ["--no-sandbox"],
@@ -17,16 +27,19 @@ const client = new Client({
 
 client.initialize();
 
-client.on('loading_screen', (percent, message) => {
-    console.log('LOADING SCREEN', percent, message);
-});
-
 client.on("qr", (qr) => {
   qrcode.generate(qr, { small: true });
 });
 
-client.on('authenticated', () => {
-    console.log('AUTHENTICATED');
+client.on('authenticated', (session) => { 
+  sessionData = session;
+  fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), (err) => { 
+    if (err) { 
+      console.error(err); 
+    } else {
+      console.log('session saved');
+    }
+  });
 });
 
 client.on('auth_failure', msg => {
