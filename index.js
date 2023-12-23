8521b1b8b702af118edd80ff4b65b138dac12560
Muhamad Abi Saleh <92934849+abiisaleh@users.noreply.github.com>
2023-12-23 11:40:46 +0900

    update index.js

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 require("dotenv").config({ path: "./.env" });
 const qrcode = require("qrcode-terminal");
 
-const { Client, LocalAuth} = require("whatsapp-web.js");
-const client = new Client({
-  authStrategy: new LocalAuth(),
+const fs = require('fs'); 
+const { Client, LegacySessionAuth } = require('whatsapp-web.js'); 
+
+// Path where the session data will be stored 
+const SESSION_FILE_PATH = './session.json'
+
+// Load the session data if it has been previously saved 
+let sessionData; 
+if(fs.existsSync(SESSION_FILE_PATH)) { sessionData = require(SESSION_FILE_PATH);
+}
+
+// Use the saved values 
+const client = new Client({ 
+  authStrategy: new LegacySessionAuth({ 
+    session: sessionData 
+  }),
   puppeteer: {
     args: ["--no-sandbox"],
     browserWSEndpoint: `wss://chrome.browserless.io?token=${process.env.browserless_api}`,
   },
+}); 
+
+// Save session values to the file upon successful auth 
+client.on('authenticated', (session) => { 
+  sessionData = session; 
+  fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), (err) => { 
+    if (err) { 
+      console.error(err);
+    } else {
+      console.log("Session saved");
+    }
+  });
 });
 
 client.on("qr", (qr) => {
