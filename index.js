561f2ac1bd3ef35c3aed5cfcfa5cc735848dc88f
Muhamad Abi Saleh <92934849+abiisaleh@users.noreply.github.com>
2023-12-23 12:53:22 +0900

    update index.js

--- index.js
+++ index.js
@@ -15,8 +15,23 @@ const client = new Client({
   },
 }); 
 
+client.initialize();
+
+client.on('loading_screen', (percent, message) => {
+    console.log('LOADING SCREEN', percent, message);
+});
+
 client.on("qr", (qr) => {
   qrcode.generate(qr, { small: true });
+});
+
+client.on('authenticated', () => {
+    console.log('AUTHENTICATED');
+});
+
+client.on('auth_failure', msg => {
+    // Fired if session restore was unsuccessful
+    console.error('AUTHENTICATION FAILURE', msg);
 });
 
 client.on("ready", () => {
@@ -37,5 +52,3 @@ client.on("message", (msg) => {
     chatBot(msg.body);
   }
 });
-
-client.initialize();
